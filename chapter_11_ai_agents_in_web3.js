0f73a65545b58764c901233b37bc87faca778d36
AI Books <ai.books.market@gmail.com>
2025-06-22 23:22:01 -0400

    Really fix indentation

--- chapter_11_ai_agents_in_web3.js
+++ chapter_11_ai_agents_in_web3.js
@@ -5,12 +5,12 @@ const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 
 // Function to complete a transaction
 async function buyNFT(buyer, seller, price) {
-	const transaction = await web3.eth.sendTransaction({
-                from: buyer,
-                to: seller,
-                value: web3.utils.toWei(price, 'ether')
-        });
-        console.log("Transaction successful:", transaction);
+    const transaction = await web3.eth.sendTransaction({
+        from: buyer,
+        to: seller,
+        value: web3.utils.toWei(price, 'ether')
+    });
+    console.log("Transaction successful:", transaction);
 }
 
 // Example: Buy an NFT for 0.1 ETH
